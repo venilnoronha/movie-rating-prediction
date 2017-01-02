53a0b4c4e1c99c2c03459fd2a636905416a45e72
Venil Noronha <venil.noronha@gmail.com>
2017-01-01 20:28:22 -0800

    Basic UI.

---
+++ movie-rating-prediction/src/main/resources/static/js/app.js
@@ -0,0 +1,45 @@
+var mrpApp = angular.module('mrpApp', []);
+
+var searchCtrl = mrpApp.controller('searchCtrl', ['$rootScope', '$scope', '$http', '$timeout', function($rootScope, $scope, $http, $timeout) {
+
+    $scope.movieName = "";
+    $scope.queryTimer = null;
+
+    $scope.onMovieNameUpdate = function() {
+    	if (!!$scope.queryTimer) {
+    		$timeout.cancel($scope.queryTimer);
+    		$scope.queryTimer = null;
+    	}
+    	$scope.queryTimer = $timeout(function() {
+    		$scope._performQuery();
+    		$scope.queryTimer = null;
+    	}, 1000);
+    };
+
+    $scope._performQuery = function() {
+    	$http
+			.get('http://www.omdbapi.com/?t=' + $scope.movieName + '&r=json')
+			.then(function(response) {
+				$rootScope.$broadcast('search-result', response.data);
+			});
+    };
+
+}]);
+
+var searchResultCtrl = mrpApp.controller('searchResultCtrl', ['$scope', function($scope) {
+
+	$scope.movieTitle = "Movie not found!";
+	$scope.moviePoster = "";
+
+	$scope.$on('search-result', function(e, data) {
+		if (!data || !data.Title) {
+			$scope.movieTitle = "Movie not found!";
+			$scope.moviePoster = "";
+		}
+		else {
+			$scope.movieTitle = data.Title + " (" + data.Year + ")";
+			$scope.moviePoster = data.Poster;
+		}
+	});
+
+}]);
